7d7b771af5cd990a09e0ff5d91ad28894213c07a
zhangBN2001 <zhangbeining@genomics.cn>
2022-09-20 09:47:05 +0800

    Update 03.plot.R

--- geom_density_ridges_gradient/03.plot.R
+++ geom_density_ridges_gradient/03.plot.R
@@ -1,11 +1,13 @@
 library(ggplot2)
 library(ggridges)
 theme_set(theme_minimal())
-list_A<-read.table("lg2.list.csv",header = F,sep = ",")
+list_A<-read.table("1lg2.list.csv",header = F,sep = ",")
+list_A<-data.frame(p.value=list_A[,"V7"],class=list_A[,"V5"])
 # Change the density area fill colors by groups
 plo<-ggplot(
   list_A, 
   aes(x = `p.value`, y = `class`,fill =stat(x))
 )
 
-plo+geom_density_ridges_gradient(scale = 1, size = 0.3, rel_min_height = 0.00)+scale_fill_viridis_c(option = "C")
+plo<-plo+geom_density_ridges_gradient(scale = 1, size = 0.3, rel_min_height = 0.00)+scale_fill_viridis_c(option = "C")
+plo+scale_x_continuous(limits = c(-20,200))
